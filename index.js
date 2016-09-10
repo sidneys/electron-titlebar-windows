2034bc5eedd3140918110eade641573783b06ed4
sidneys <sidneys.github.io@outlook.com>
2016-09-11 00:55:44 +0200

    v1.0.3

--- index.js
+++ index.js
@@ -1,65 +1,119 @@
-var events = require('events');
-var util = require('util');
-var fs = require('fs');
-var defaultcss = require('defaultcss');
-var domify = require('domify');
-var $ = require('dombo');
+'use strict';
 
-var ALT = 18;
+/**
+ * Modules: Node
+ * @global
+ */
+var path = require('path'),
+    fs = require('fs'),
+    util = require('util');
+
+
+/**
+ * Modules: Third Party
+ * @global
+ */
+var events = require('events'),
+    defaultCss = require('defaultcss'),
+    domify = require('domify'),
+    $ = require('dombo');
+
+
+/**
+ * Init
+ * @global
+ */
+
 var $window = $(window);
 
-var style = fs.readFileSync(__dirname + '/index.css', 'utf-8');
-var html = fs.readFileSync(__dirname + '/index.html', 'utf-8');
+
+/**
+ * HTML/CSS Payloads
+ * @global
+ */
+var style = fs.readFileSync(__dirname + '/index.css', 'utf-8'),
+    html = fs.readFileSync(__dirname + '/index.html', 'utf-8');
 
 
+/**
+ * TitleBar
+ */
 var TitleBar = function(options) {
-    if (!(this instanceof TitleBar)) return new TitleBar(options);
+    if (!(this instanceof TitleBar)) {
+        return new TitleBar(options);
+    }
 
     events.EventEmitter.call(this);
+
     this._options = options || {};
 
-    var element = domify(html);
-    var $element = $(element);
+    var element = domify(html, document),
+        $element = $(element);
+                                                                          0
     this.element = element;
 
     // Option: draggable
-    if (this._options.draggable !== false) $element.addClass('webkit-draggable');
+    if (this._options.draggable !== false) {
+        $element.addClass('webkit-draggable');
+    }
 
     var self = this;
-    var close = $('.titlebar-close', element)[0];
-    var maximize = $('.titlebar-maximize', element)[0];
-    var minimize = $('.titlebar-minimize', element)[0];
-    var fullscreen = $('.titlebar-fullscreen', element)[0];
 
-    $element.on('click', function(e) {
-        console.log('element', e);
-        var target = e.target;
-        if (close.contains(target)) {
-            self.emit('close', e);
-        } else if (minimize.contains(target)) {
-            self.emit('minimize', e);
-        } else if (maximize.contains(target)) {
-            self.emit('maximize', e);
-        } else if (fullscreen.contains(target)) {
-            if (e.altKey) {
-                self.emit('maximize', e);
+    var closeButton = $('.titlebar-close', element)[0],
+        maximizeButton = $('.titlebar-maximize', element)[0],
+        minimizeButton = $('.titlebar-minimize', element)[0],
+        fullscreenButton = $('.titlebar-fullscreen', element)[0];
+
+    $element.on('click', function(ev) {
+
+        var target = ev.target;
+
+        if (closeButton && closeButton.contains(target)) {
+            return self.emit('close', ev);
+        }
+
+        if (minimizeButton && minimizeButton.contains(target)) {
+            return self.emit('minimize', ev);
+        }
+
+        if (maximizeButton && maximizeButton.contains(target)) {
+            return self.emit('maximize', ev);
+        }
+
+        if (fullscreenButton && fullscreenButton.contains(target)) {
+            if (ev.altKey) {
+                self.emit('maximize', ev);
             } else {
-                self.emit('fullscreen', e);
+                self.emit('fullscreen', ev);
             }
         }
     });
 
-    $element.on('dblclick', function(e) {
-        var target = e.target;
-        if (close.contains(target) || maximize.contains(target) || minimize.contains(target) || fullscreen.contains(target)) return;
-        self.emit('maximize', e);
+    $element.on('dblclick', function(ev) {
+        var target = ev.target;
+
+        if (closeButton.contains(target) ||
+            maximizeButton.contains(target) ||
+            minimizeButton.contains(target) ||
+            fullscreenButton.contains(target)) {
+            return;
+        }
+
+        self.emit('maximize', ev);
     });
 };
 
+
 util.inherits(TitleBar, events.EventEmitter);
 
+
+/**
+ * TitleBar#appendTo
+ */
 TitleBar.prototype.appendTo = function(target) {
-    if (typeof target === 'string') target = $(target)[0];
+    if (typeof target === 'string') {
+        target = $(target)[0];
+    }
 
     var $element = $(this.element);
 
@@ -74,28 +128,44 @@ TitleBar.prototype.appendTo = function(target) {
     }
 
     // Option: style
-    if (this._options.style !== false) defaultcss('titlebar', style);
+    if (this._options.style !== false) {
+        defaultCss('titlebar', style);
+    }
 
-
-    $window.on('keydown', this._onkeydown = function(e) {
-        if (e.keyCode === ALT) $element.addClass('alt');
+    $window.on('keydown', this._onkeydown = function(ev) {
+        if (ev.keyCode === 18) {
+            $element.addClass('alt')
+        }
     });
 
-    $window.on('keyup', this._onkeyup = function(e) {
-        if (e.keyCode === ALT) $element.removeClass('alt');
+    $window.on('keyup', this._onkeyup = function(ev) {
+        if (ev.keyCode === 18) {
+            $element.removeClass('alt');
+        }
     });
 
     target.appendChild(this.element);
+
     return this;
 };
 
+
+/**
+ * TitleBar#destroy
+ */
 TitleBar.prototype.destroy = function() {
     var parent = this.element.parentNode;
-    if (parent) parent.removeChild(this.element);
+
+    if (parent) {
+        parent.removeChild(this.element);
+    }
+
     $window.off('keydown', this._onkeydown);
     $window.off('keyup', this._onkeyup);
+
     return this;
 };
+
 
 /*
  * @exports
